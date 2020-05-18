6c5457e34806fb9fa6570a73784140b3ca663416
temmenie <48475024+temmenie@users.noreply.github.com>
2020-05-18 13:54:39 +0200

    Add project files.

---
+++ invulForm.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySQLdataAdapterApp
+{
+    public partial class invulForm : Form
+    {
+        public event EventHandler<List<string>> wijzigingenOpslaan;
+        public event EventHandler<List<string>> nieuwRecordOpslaan;
+
+        private int mode = -1;
+
+        public invulForm()
+        {
+            InitializeComponent();
+        }
+
+        public void recordAanpassen(int row, string productnaam, string stock, string beschikbaarheid)
+        {
+            label4.Text = "Record aanpassen";
+            this.Text = "MySQL Databasebeheer - record aanpassen";
+
+            mode = row;
+            textBox1.Text = productnaam;
+            textBox2.Text = stock;
+            if (beschikbaarheid == "1")
+            {
+                checkBox1.Checked = true;
+            }
+            else
+            {
+                checkBox1.Checked = false;
+            }
+        }
+
+        public void recordToevoegen()
+        {
+            label4.Text = "Record Toevoegen";
+            this.Text = "MySQL Databasebeheer - record toevoegen";
+
+            mode = -1;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            checkBox1.Checked = false;
+        }
+
+        protected virtual void OnWijzigingenOpslaan(List<string> e)
+        {
+            if (wijzigingenOpslaan != null)
+            {
+                wijzigingenOpslaan.Invoke(this, e);
+            }
+        }
+
+        protected virtual void OnNieuwRecordOpslaan(List<string> e)
+        {
+            if (nieuwRecordOpslaan != null)
+            {
+                nieuwRecordOpslaan.Invoke(this, e);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            int val;
+            if (Int32.TryParse(textBox2.Text, out val))
+            {
+                List<string> temp = new List<string>();
+                temp.Add(textBox1.Text);
+                temp.Add(textBox2.Text);
+                if (checkBox1.Checked)
+                {
+                    temp.Add("1");
+                }
+                else
+                {
+                    temp.Add("0");
+                }
+
+                if (mode != -1)
+                {
+                    temp.Add(mode.ToString());
+                    OnWijzigingenOpslaan(temp);
+                }
+                else
+                {
+                    OnNieuwRecordOpslaan(temp);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Dit is geen getal: " + textBox2.Text);
+            }
+        }
+
+        private void invulForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+
+        private void invulForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ mainForm.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace MySQLdataAdapterApp
+{
+    public partial class mainForm : Form
+    {
+        //objecten van belang bij het maken van een connectie met de database
+        MySqlConnection myConnection;
+        MySqlDataAdapter myDataAdapter;
+        MySqlCommandBuilder myCommandBuidler;
+        DataTable myTable;
+
+        string connectionString;
+
+        string selectQuery = "SELECT * FROM producten";
+        string toggleForeign_key_checks_uit = "set foreign_key_checks = 0;";
+        string toggleForeign_key_checks_aan = "set foreign_key_checks = 1;";
+
+        invulForm invulForm = new invulForm();
+        public mainForm()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["MySQLConnection"].ConnectionString;
+            invulForm.wijzigingenOpslaan += InvulFormOnWijzigingenOpslaan;
+            invulForm.nieuwRecordOpslaan += InvulFormOnNieuwRecordOpslaan;
+        }
+
+        private void BtnExecuteSelectQuery_Click(object sender, EventArgs e)
+        {
+            myConnection = new MySqlConnection(connectionString);
+            using (myDataAdapter = new MySqlDataAdapter(selectQuery, myConnection))
+            {
+                myCommandBuidler = new MySqlCommandBuilder(myDataAdapter);
+                myTable = new DataTable();
+                myDataAdapter.Fill(myTable);
+                DgvProducten.DataSource = myTable;
+                myTable.AcceptChanges();
+            }
+
+            DgvProducten.ClearSelection();
+            BtnRecordVerwijderen.Enabled = false;
+            BtnRecordWijzigen.Enabled = false;
+        }
+
+        private void BtnUpdateTabel_Click(object sender, EventArgs e)
+        {
+            DataTable myChanges = myTable.GetChanges();
+            if (myChanges == null)
+            {
+                MessageBox.Show("Er is niets veranderd.");
+            }
+            else
+            {
+                MySqlCommand cmdOff = new MySqlCommand();
+                MySqlCommand cmdOn = new MySqlCommand();
+
+                cmdOff.CommandText = toggleForeign_key_checks_uit;
+                cmdOff.Connection = myConnection;
+                cmdOn.CommandText = toggleForeign_key_checks_aan;
+                cmdOn.Connection = myConnection;
+
+                myConnection.Open();
+                cmdOff.ExecuteNonQuery();
+
+                using (myDataAdapter = new MySqlDataAdapter(selectQuery, myConnection))
+                {
+                    myCommandBuidler = new MySqlCommandBuilder(myDataAdapter);
+                    myDataAdapter.Update(myChanges);
+                    myTable.AcceptChanges();
+                }
+
+                cmdOn.ExecuteNonQuery();
+                myConnection.Close();
+            }
+
+            BtnRecordVerwijderen.Enabled = true;
+            BtnRecordWijzigen.Enabled = true;
+        }
+
+        private void DgvProducten_DoubleClick(object sender, EventArgs e)
+        {
+            DataGridViewSelectedRowCollection geselecteerdeRijen = DgvProducten.SelectedRows;
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (DataGridViewRow r in geselecteerdeRijen)
+            {
+                sb.Append(r.Index.ToString());
+            }
+
+            BtnRecordVerwijderen.Enabled = true;
+            BtnRecordWijzigen.Enabled = true;
+
+            MessageBox.Show("rij "+ (Int32.Parse(sb.ToString())+1) + " geselecteerd...");
+        }
+
+        private void PasDataTabelAan(DataTable tabel, int rij, int kol, string data)
+        {
+            if (rij < tabel.Rows.Count && kol < tabel.Columns.Count)
+            {
+                tabel.Rows[rij][kol] = data;
+            }
+        }
+
+        private void verwijderRecordData(DataGridView grid,int rij)
+        {
+            if (rij < grid.RowCount)
+            {
+                grid.Rows.RemoveAt(rij);
+                BtnRecordVerwijderen.Enabled = false;
+                BtnRecordWijzigen.Enabled = false;
+            }
+        }
+
+        private void BtnRecordVerwijderen_Click(object sender, EventArgs e)
+        {
+            var confirmResult = MessageBox.Show(text: "Ben je zeker dat je " + 
+                                                       DgvProducten.SelectedRows[0].Cells[1].Value + 
+                                                       " wilt verwijderen? ", "Zeker delete?", MessageBoxButtons.YesNo);
+            if(confirmResult == DialogResult.Yes)
+            {
+                verwijderRecordData(DgvProducten, DgvProducten.SelectedRows[0].Index);
+            }
+        }
+
+        private void BtnRecordWijzigen_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow temp = DgvProducten.SelectedRows[0];
+            invulForm.recordAanpassen(temp.Index, temp.Cells[1].Value.ToString(),temp.Cells[2].Value.ToString(), temp.Cells[3].Value.ToString());
+
+            invulForm.Show();
+            invulForm.BringToFront();
+        }
+
+        private void BtnRecordToevoegen_Click(object sender, EventArgs e)
+        {
+            invulForm.recordToevoegen();
+            invulForm.Show();
+            invulForm.BringToFront();
+        }
+
+        private void InvulFormOnNieuwRecordOpslaan(object sender, List<string> e)
+        {
+            myTable.Rows.Add();
+            PasDataTabelAan(myTable, (myTable.Rows.Count)-1, 1, e[0]);
+            PasDataTabelAan(myTable, (myTable.Rows.Count) - 1, 2, e[1]);
+            PasDataTabelAan(myTable, (myTable.Rows.Count) - 1, 3, e[2]);
+            invulForm.Hide();
+        }
+
+        private void InvulFormOnWijzigingenOpslaan(object sender, List<string> e)
+        {
+            int rij = Int32.Parse(e[3]);
+            PasDataTabelAan(myTable, rij, 1, e[0]);
+            PasDataTabelAan(myTable, rij, 2, e[1]);
+            PasDataTabelAan(myTable, rij, 3, e[2]);
+            invulForm.Hide();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
